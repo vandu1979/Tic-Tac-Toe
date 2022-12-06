e187af76b0205e0ef4bd90c6cfdb13fe51374c70
vandu1979 <vandana_kothari@yahoo.com>
2022-12-06 11:10:57 -0700

    Hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Player from './components/Player';
+import Board  from './components/Board';
+
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Header />
+    <Player whichPlayer='X'/>
+    <Player whichPlayer='O'/>
+    <Board />
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Square(props) {
+  return (
+    <div>
+        <h4>Square</h4>
+    </div>
+  )
+}
+
+export default Square
